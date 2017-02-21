2de1b49bcf9cef7cf7da6997a9f7ddc4e0379c39
Douglas Orr <douglas.orr99@gmail.com>
2017-02-21 15:26:47 +0000

    Increment version

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@
 from distutils.core import setup
 
 setup(name='DeepLearnTute',
-      version='0.3',
+      version='0.4',
       packages=['dlt'],
       package_data={'dlt': ['custom_input.js']})
