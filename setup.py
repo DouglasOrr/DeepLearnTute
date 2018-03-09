c5040cb8f80e4c7499c1439e0258aa2b56c9a9c6
Douglas Orr <douglas.orr99@gmail.com>
2018-03-09 18:37:24 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@
 from distutils.core import setup
 
 setup(name='DeepLearnTute',
-      version='0.6',
+      version='0.7',
       packages=['dlt'],
       package_data={'dlt': ['custom_input.js']})
